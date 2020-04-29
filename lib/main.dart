2a082e11400274a22803fce686efc14bdc3a0b77
EdFilho <j.edvaniofilho@gmail.com>
2020-04-28 23:48:47 -0300

    teste

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:solar_system/home/splash_page.dart';
 import 'package:solar_system/themes/themes_app.dart';
-
+//test3
 void main() {
   runApp(
     new MaterialApp(

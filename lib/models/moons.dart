52423ed1e08157ef67c795627f15e8b2f77384e3
EdFilho <j.edvaniofilho@gmail.com>
2020-05-01 17:17:23 -0300

    moon model

---
+++ lib/models/moons.dart
@@ -0,0 +1,32 @@
+class Moon {
+  final String id;
+  final String name;
+  final String planet;
+  final String size;
+  final String volume;
+  final String density;
+  final String description;
+  final String nomeclature;
+  final String discovered;
+  final String structure;
+  final String formation;
+
+  const Moon ({ this.id, this.name, this.planet, this.size, this.volume, this.density, this.description, this.nomeclature, this.discovered, this.structure, this.formation });
+
+}
+
+List <Moon> moons = [
+  const Moon(
+    id: '1',
+    name: 'Lua',
+    planet: 'Terra',
+    size: '37.936.694,79 km²',
+    volume: '21.971.669.064 km³',
+    density: '3,344 g/cm³',
+    description: 'bla bla bla',
+    nomeclature: 'bla bla',
+    discovered: 'bla bla',
+    structure: 'bla bla',
+    formation: 'bla bla',
+  ),
+];
